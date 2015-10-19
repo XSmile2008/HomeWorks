c1361b5781d69aa8729a0bc730f44bc90a255e00
XSmile2008 <XSmile2008@gmail.com>
2015-10-19 19:01:52 +0300

    just commit

--- app/src/main/java/com/vstarikov/homeworks/third/ThirdActivity.java
+++ app/src/main/java/com/vstarikov/homeworks/third/ThirdActivity.java
@@ -1,7 +1,14 @@
 package com.vstarikov.homeworks.third;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Starikov on 19.10.15.
  */
-public class ThirdActivity {
+public class ThirdActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
